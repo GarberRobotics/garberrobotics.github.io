dd73016ffa62cb658ba0882b84d801cde3b29fac
Nick <20Nick@users.noreply.github.com>
2018-06-07 20:04:33 -0400

    testing res

--- js/gallery.js
+++ js/gallery.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
     height = document.getElementById('imageGal').height;
 });
 
+document.getElementById("aboutText").style.padding = "10px 10px 10px " + width+"px";
+
 var imageSources = ["5216TeamMember2018.jpeg", "awardMidland.png", "awardMidland1.png", "comp.jpg", "compEndGame.png", "compGame.jpg", "Driving.png", "pits.png", "teamPicture.png",
 "20180324_145929.gif", "20180427_161139_resized.png", "20180427_152009_resized.jpg", "20180414_112920_resized.png"]
 
