871d117f25ca8d31fc41bf77187705ad7b41b807
Nick <20Nick@users.noreply.github.com>
2018-06-13 15:38:50 -0400

    Added 404 page, combined robots and awards

--- README.md
+++ README.md
@@ -3,12 +3,12 @@ E-Ville Empire FRC Team 5216
 
 
 ## Things To do
-* make a 404 error page
 * Add our Sponsors
 * Add our mentors with pictures and contact info
-* Do something with the gallery
+* add something with the gallery
 * add pictures of old robot and there name
-* add the awards that we have won
+* ~~add the awards that we have won~~
+* ~~make a 404 error page~~ do the 404 error page better
 * ~~Fix Mobile Version~~ Still could use a lot of work, but fixed.
 * ~~Commit my version with the Star wars stylesheet added~~
 * ~~add favicon~~
--- js/gallery.js
+++ js/gallery.js
@@ -15,9 +15,11 @@ $(document).ready(function() {
 });
 
 
-
+// IMPORTANT!!! MAKE SURE THE IMAGES ARE 1200x800 !!!!
+//To add images to the slideshow add put the image in the images folder and put the filename here
 var imageSources = ["comp.jpg", "compEndGame.png", "compGame.jpg", "5216TeamMember2018.jpeg", "awardMidland.png", "awardMidland1.png", "Driving.png", "pits.png", "teamPicture.png",
 "20180324_145929.gif", "20180427_161139_resized.png", "20180427_152009_resized.jpg", "20180414_112920_resized.png"]
+// IMPORTANT!!! MAKE SURE THE IMAGES ARE 1200x800 !!!!
 
 function startSlideShow(){
   var index = 0;
@@ -29,5 +31,5 @@ function startSlideShow(){
     //document.getElementById("imageGal").style.width = width+"px";
     document.getElementById("imageGal").style.height = height+"px";
     index++;
-  } , 10000); // 10,000 is 10 sec be for changing pics
+  }, 10000); // 10,000 is 10 sec be for changing pics
 }
